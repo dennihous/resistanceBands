4189847aedc70235891261e6e343872629d9e3e0
dennis <denhouston1@icloud.com>
2023-09-30 11:18:36 +0100

    Cart Item added but removed when page refresh

--- src/features/Cart/Cart.tsx
+++ src/features/Cart/Cart.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import bandData from '../../../bandsData.json';
+import { ShopContext } from '../context/ShopContext';
+import CartItem from './CartItem';
 
 export default function Cart() {
+  const { cartItems }: any = useContext(ShopContext)
   return (
-    <div>Cart</div>
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cartItems">
+        {bandData.bands.map((band) => {
+          if (cartItems[band.id] !== 0) {
+            return <CartItem band={band} />
+          }
+        })}
+      </div>
+    </div>
   )
 }
---
+++ src/features/Cart/CartItem.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CartItem({band}: any) {
+  return (
+    <div>{band.id}</div>
+  )
+}
