569deedd9131dd5aca5ebf1ef3433d2376d37b50
dennis <denhouston1@icloud.com>
2023-10-12 17:36:34 +0100

    Adding testing navbar to see if context will work

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
-import NavBar from './features/NavBar/NavBar';
 import ErrorPage from './pages/error-page';
 import './index.scss';
 import BandPage, { loader as bandPageLoader} from './pages/BandPage';
 import CartPage from './pages/CartPage';
 import { ShopContextProvider } from './features/context/ShopContext';
+import TestNavBar from './features/NavBar/testNavBar';
 
 function App() {
   return (
     <div className="App">
       <ShopContextProvider>
         <BrowserRouter>
-          <NavBar />
+          <TestNavBar />
           <Routes>
             <Route path="/" element={<Home />} errorElement={<ErrorPage />} />
             <Route path="/bands/:bandId" element={<BandPage />} loader={bandPageLoader} errorElement={<ErrorPage />} />
