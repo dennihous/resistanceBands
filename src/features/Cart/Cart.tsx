05c966e680cf489e06dab4f4bb667e00c49f5d63
dennis <denhouston1@icloud.com>
2023-10-12 17:37:17 +0100

    Adding new cart with improved context abilities

--- src/features/Cart/Cart.tsx
+++ src/features/Cart/Cart.tsx
@@ -1,22 +1,45 @@
 import React, { useContext } from 'react';
-import bandData from '../../../bandsData.json';
+import bands from '../../../bandsData.json';
 import { ShopContext } from '../context/ShopContext';
 import CartItem from './CartItem';
+import { useNavigate } from "react-router-dom";
 
 export default function Cart() {
-  const { cartItems }: any = useContext(ShopContext)
+  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
   return (
     <div className="cart">
       <div>
         <h1>Your Cart Items</h1>
       </div>
-      <div className="cartItems">
-        {bandData.bands.map((band) => {
-          if (cartItems[band.id] !== 0) {
-            return <CartItem band={band} />
+      <div className="cart">
+        {bands.bands.map((product) => {
+          if ((cartItems as any)[product.id] !== 0) {
+            return <CartItem data={product} />;
           }
         })}
       </div>
+
+      {totalAmount > 0 ? (
+        <div className="checkout">
+          <p> Subtotal: ${totalAmount} </p>
+          <button onClick={() => navigate("/")}> Continue Shopping </button>
+          <button
+            onClick={() => {
+              checkout();
+              navigate("/checkout");
+            }}
+          >
+            {" "}
+            Checkout{" "}
+          </button>
+        </div>
+      ) : (
+        <h1> Your Shopping Cart is Empty</h1>
+      )}
     </div>
-  )
-}
+  );
+};
