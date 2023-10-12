43916f2a7ea9d945330a3f8e19859f92d21d4a37
dennis <denhouston1@icloud.com>
2023-10-12 16:53:53 +0100

    Adding changed react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 import NavBar from './features/NavBar/NavBar';
@@ -13,34 +10,26 @@ import BandPage, { loader as bandPageLoader} from './pages/BandPage';
 import CartPage from './pages/CartPage';
 import { ShopContextProvider } from './features/context/ShopContext';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <NavBar />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/bands/:bandId",
-        element: <BandPage />,
-        loader: bandPageLoader,
-      },
-      {
-        path: "/cart",
-        element: <CartPage />
-      },
-    ]
-  }
-])
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} errorElement={<ErrorPage />} />
+            <Route path="/bands/:bandId" element={<BandPage />} loader={bandPageLoader} errorElement={<ErrorPage />} />
+            <Route path="/cart" element={<CartPage />} errorElement={<ErrorPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ShopContextProvider>
+    </div>
+  )
+}
 
-const container = document.getElementById('root');
-ReactDOM.createRoot(container as Element).render(
+const root = ReactDOM.createRoot(document.getElementById("root") as Element);
+root.render(
   <React.StrictMode>
-    <ShopContextProvider>
-      <RouterProvider router={router} />
-    </ShopContextProvider>
+    <App />
   </React.StrictMode>
-)
+);
