10342696f8338e1f4b30194e43edaf035707ed80
dennis <denhouston1@icloud.com>
2023-10-13 14:52:00 +0100

    Adding a testNavbar

--- src/features/NavBar/TestNavBar.tsx
+++ src/features/NavBar/TestNavBar.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
 import Image from 'react-bootstrap/Image';
-import "./testNavBar.scss";
+import "./TestNavBar.scss";
 
 export default function TestNavBar() {
   return (
     <div className="navbar">
-      <div className="links">
+      <Link to="/">
         <Image
           src="/images/Icon-removed-background.png"
           alt="Logo"
           className="resistance-band-logo-image"
         />
+      </Link>
+      <div className="links">
         <Link to="/"> Resistance Bands </Link>
         <Link to="/cart">
           <ShoppingCart size={32} />
