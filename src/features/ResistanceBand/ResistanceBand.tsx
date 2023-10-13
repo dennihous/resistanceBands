a812071b27264e53201b4e62b9da418aa55e7706
dennis <denhouston1@icloud.com>
2023-10-13 14:52:00 +0100

    Small refactor of resistance bands

--- src/features/ResistanceBand/ResistanceBand.tsx
+++ src/features/ResistanceBand/ResistanceBand.tsx
@@ -18,13 +18,13 @@ export default function ResistanceBand({ name, imageUrl, price, id }: Resistance
     <div className='product'>
       <Link to={`bands/${id}`}>
         <img src={imageUrl} alt={name} className="resistance-band-image" />
+      </Link>
         <div className="description">
           <p>
             <b>{name}</b>
           </p>
           <p>£{price}</p>
         </div>
-      </Link>
       <Button className='add-to-cart-button' onClick={() => addToCart(id)}>
         Add to Cart {cartItemAmount > 0 && <>({cartItemAmount})</>}</Button>
     </div>
