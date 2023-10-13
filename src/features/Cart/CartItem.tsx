a6f746d659991e69fa2e8ee563ecc162d9b8f111
dennis <denhouston1@icloud.com>
2023-10-13 14:52:00 +0100

    Small refactor of cartItem

--- src/features/Cart/CartItem.tsx
+++ src/features/Cart/CartItem.tsx
@@ -1,26 +1,28 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
+import './CartItem.scss';
 
 export default function  CartItem(props: any) {
-  const { id, productName, price, productImage } = props.data;
+  const { id, name, price, imageUrl } = props.data;
   const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
     useContext(ShopContext);
 
   return (
     <div className="cartItem">
-      <img src={productImage} />
+      <img src={imageUrl} className="cartItemImage" />
       <div className="description">
         <p>
-          <b>{productName}</b>
+          <b>{name}</b>
         </p>
-        <p> Price: ${price}</p>
+        <p> Price: £{price}</p>
         <div className="countHandler">
-          <button onClick={() => removeFromCart(id)}> - </button>
+          <button onClick={() => removeFromCart(id)} className="cartItemButton"> - </button>
           <input
             value={(cartItems as any)[id]}
             onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+            className="cartItemQuantity"
           />
-          <button onClick={() => addToCart(id)}> + </button>
+          <button onClick={() => addToCart(id)} className="cartItemButton"> + </button>
         </div>
       </div>
     </div>
